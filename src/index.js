9152a22eb21c1f8a4fc27359394d15e2dbb82151
WerHawer <werhawer@gmail.com>
2020-12-29 13:48:46 +0200

    add title

--- src/index.js
+++ src/index.js
@@ -3,28 +3,22 @@ import catalogList from './catalog.hbs';
 import result from './result.json';
 
 const root = document.querySelector('#root');
+const title = document.querySelector('.title');
 
-let finish = 10;
+title.insertAdjacentHTML('beforeend', result.collection);
+
+const elementsOnPage = 10;
 const products = [...result.products];
-console.log('products', products);
-const max = products.length + 10;
-console.log('catalog', max);
 
-for (let i = 0; i < max; i += 10) {
+const max = products.length;
+
+for (let i = 0; i < max; i += elementsOnPage) {
+  const finish = i + elementsOnPage;
+
   const part = result.products.slice(i, finish);
+
   const newCatalog = { products: [...part] };
 
   const list = catalogList(newCatalog);
   root.insertAdjacentHTML('beforeend', list);
-
-  finish += 10;
 }
-
-let num = [];
-
-for (let i = 0; i <= 70; i++) {
-  if (i % 2 !== 0) {
-    num.push(i);
-  }
-}
-console.log('num', num.join(','));
